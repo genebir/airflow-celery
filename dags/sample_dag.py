34b4a09ba04c5eea72e0cf1e9d6c0802481b3998
genebir <duarltmd1@naver.com>
2023-09-06 17:31:23 +0900

    first commit

---
+++ dags/sample_dag.py
@@ -0,0 +1,22 @@
+from airflow.decorators import dag, task
+from airflow.operators.empty import EmptyOperator
+from datetime import datetime
+from pendulum import timezone
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 1, 1).astimezone(tz=timezone('Asia/Seoul')),
+    'catchup': False,
+}
+
+@dag(default_args=default_args, schedule_interval=None, tags=['example'])
+def sample_dag():
+    @task(task_id='task_1')
+    def task_1():
+        print('task_1')
+
+    start = EmptyOperator(task_id='start')
+    end = EmptyOperator(task_id='end')
+
+    start >> task_1() >> end
+sample_dag()
